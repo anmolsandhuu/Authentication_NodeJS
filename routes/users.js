5a78540de6b772a33cdd2530deab597a3099f709
Anmol Sandhu <assandhu23@myseneca.ca>
2019-07-02 10:00:30 -0400

    fixed user.js routing

--- routes/users.js
+++ routes/users.js
@@ -7,33 +7,35 @@ router.get('/login', (req, res) =>res.render('login'));
 router.get('/register', (req, res) =>res.render('register'));
 
 // Register handle
-router.post('/register', (req,res)=>{
+router.post('/register', (req, res)=> {
     const { name, email, password, password_ } = req.body;
     console.log(req.body);
     let errors = [];
 
     //checking
     if(!name || !email || !password || !password_){
-        errors.push({ msg: "Please fill all fields."});
-    }
+        errors.push({ msg: 'Please fill all fields'});
+    };
+    if(!password.length < 6)
+    {
+        errors.push({ msg: 'Password are less than 6 characters'});
+    };
     if(!password !== password_)
     {
-        errors.push({ msg: "Password doo not match! Are you stupid!"});
-    }
-    if(!password.length< 6)
-    {
-        errors.push({ msg: "Password are less than 6 characters"});
-    }
+        errors.push({ msg: 'Password doo not match! Are you stupid!'});
+    };
+    
     if(errors.length > 0){
-        res.render('register',{
+        res.render('register', {
             errors,
             name,
             email,
             password,
             password_
         });
-    }else{
+    } 
+    else {
         res.send('Granted');
-    }
+    };
 });
 module.exports =  router;
