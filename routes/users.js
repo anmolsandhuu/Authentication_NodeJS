8b3a9598e59ac0a7080cd9734d3e7a658e8258ae
Anmol Sandhu <assandhu23@myseneca.ca>
2019-07-03 01:09:17 -0400

    register still not working

--- routes/users.js
+++ routes/users.js
@@ -8,21 +8,23 @@ router.get('/register', (req, res) =>res.render('register'));
 
 // Register handle
 router.post('/register', (req, res)=> {
+    
+    let errors = [];
     const { name, email, password, password_ } = req.body;
     console.log(req.body);
-    let errors = [];
+ 
 
     //checking
     if(!name || !email || !password || !password_){
-        errors.push({ msg: 'Please fill all fields'});
+        errors.push({ "msg": '1231231243'});
     };
     if(!password.length < 6)
     {
-        errors.push({ msg: 'Password are less than 6 characters'});
+        errors.push({ "msg": 'P123123t match! Are you stupid!'});
     };
     if(!password !== password_)
     {
-        errors.push({ msg: 'Password doo not match! Are you stupid!'});
+        errors.push({ "msg": 'Password doo n32131e you stupid!'});
     };
     
     if(errors.length > 0){
@@ -35,7 +37,7 @@ router.post('/register', (req, res)=> {
         });
     } 
     else {
-        res.send('Granted');
+        res.send('PASSS')
     };
 });
 module.exports =  router;
